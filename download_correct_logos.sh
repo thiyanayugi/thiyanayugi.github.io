dfd3d2a92a2c4cff4f6139b3a3243d75c25d4346
Thiyanayugi Mariraj <yugimariraj01@gmail.com>
2025-12-11 09:44:42 +0100

    🎨 Fix Hero/Contact text visibility & add ALL assets (certificates, reports, recommendations)

---
+++ download_correct_logos.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+cd /home/yugi/thiyanayugi.github.io/assets/img
+
+# Programming Languages
+curl -o python-logo.svg "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
+curl -o cpp-logo.svg "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg"
+curl -o matlab-logo.svg "https://upload.wikimedia.org/wikipedia/commons/2/21/Matlab_Logo.png"
+curl -o bash-logo.svg "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg"
+curl -o javascript-logo.svg "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+curl -o mysql-logo.svg "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
+
+# AI & ML
+curl -o pytorch-logo.svg "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytorch/pytorch-original.svg"
+curl -o tensorflow-logo.svg "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tensorflow/tensorflow-original.svg"
+curl -o opencv-logo.svg "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/opencv/opencv-original.svg"
+curl -o numpy-logo.svg "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/numpy/numpy-original.svg"
+curl -o pandas-logo.svg "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pandas/pandas-original.svg"
+curl -o sklearn-logo.svg "https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg"
+
+# Generative AI
+curl -o openai-logo.svg "https://upload.wikimedia.org/wikipedia/commons/4/4d/OpenAI_Logo.svg"
+curl -o huggingface-logo.svg "https://huggingface.co/front/assets/huggingface_logo.svg"
+
+# Robotics
+curl -o ros-logo.svg "https://upload.wikimedia.org/wikipedia/commons/b/bb/Ros_logo.svg"
+
+# Cloud & DevOps
+curl -o docker-logo.svg "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"
+curl -o kubernetes-logo.svg "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain.svg"
+curl -o git-logo.svg "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+curl -o linux-logo.svg "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg"
+curl -o aws-logo.svg "https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg"
+curl -o gcp-logo.svg "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/googlecloud/googlecloud-original.svg"
+
+# Universities for Education
+curl -o psg-logo.png "https://upload.wikimedia.org/wikipedia/en/9/93/PSG_College_of_Technology_logo.png"
+
+echo "Logos downloaded successfully!"
